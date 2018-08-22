a0945e13d31b6e09ebe64a108ea84273c7701dbd
José Antonio Palacios Ramírez <japalacios@ingenia.local>
2018-08-22 14:01:18 +0200

    ADD: Use yargs package

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+const argv = require('yargs')
+            .command('listar', 'Imprime en consola la tabla de multiplicar', 
+            {
+                base: { 
+                    demand: true,
+                    alias: 'b'
+                },
+                limite: {
+                    alias: 'l',
+                    default: 10
+                }
+            })
+            .help()
+            .argv;
 const multiplicar = require('./multiplicar/multiplicar');
 
-let argv = process.argv;
-let param = argv[2];
-let base = param.split('=')[1];
+// let argv = process.argv;
+// let param = argv[2];
+// let base = param.split('=')[1];
 
-multiplicar.crearArchivo( base )
-    .then( resp => console.log(resp))
-    .catch( err => console.log(err));
+
+console.log(argv.base);
+console.log(argv.limite);
+
+// multiplicar.crearArchivo( base )
+//     .then( resp => console.log(resp))
+//     .catch( err => console.log(err));
